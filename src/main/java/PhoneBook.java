b05a81b1e7a432f048d3de7dd01494645be7718c
13tom13 <13tom13@mail.ru>
2023-04-13 22:17:22 +0200

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    private Map <String, Integer> phoneBook = new HashMap<>();
+
+    public Map<String, Integer> getPhoneBook() {
+        return phoneBook;
+    }
+
+    public int add (String name, int number){
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,58 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+    PhoneBook phoneBook;
+
+    @BeforeEach
+    void beforeEach() {
+        phoneBook = new PhoneBook();
+    }
+
+
+    @Test
+    void addMethodTest() {
+        System.out.println("add method test");
+        // given:
+        String name = "Test";
+        int number = 1;
+        // when:
+        int test = phoneBook.add(name, number);
+        // then:
+        Assertions.assertTrue(phoneBook.getPhoneBook().containsKey(name));
+        System.out.println("Add test - checked");
+    }
+
+    @Test
+    void addMethodTestAmountNumbers() {
+        System.out.println("add method test: amount numbers");
+        // given:
+        String name = "Test";
+        int number = 1;
+        int amountNumber = 1;
+        // when:
+        int test = phoneBook.add(name, number);
+        // then:
+        Assertions.assertEquals(amountNumber, test);
+        System.out.println("amount numbers - checked");
+    }
+
+
+    @Test
+    void addTestThrowsException() {
+        System.out.println("add method test: throws Exception");
+        // given:
+        String name1 = "Test";
+        int number = 1;
+        // when:
+        phoneBook.add(name1, number);
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () ->
+                phoneBook.add(name1, number)
+        );
+        // then:
+        Assertions.assertEquals("Контакт с таким именем уже есть", exception.getMessage());
+        System.out.println("throws Exception - checked");
+    }
 }
